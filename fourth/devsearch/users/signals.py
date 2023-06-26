626eed9143e09fb733da88e5ec2b601561f7bf5b
Helen <elena.kozovyakina@gmail.com>
2023-06-26 21:59:51 +0300

    devsearch

--- fourth/devsearch/users/signals.py
+++ fourth/devsearch/users/signals.py
@@ -28,3 +28,20 @@ def delete_user(sender, instance, **kwargs):
 
 # post_save.connect(profile_updated, sender=Profile)
 # post_delete.connect(delete_user, sender=Profile)
+
+
+@receiver(post_save, sender=Profile)
+def update_user(sender, instance, created, **kwargs):
+    profile = instance
+    user = profile.user
+
+    if created is False:
+        user.first_name = profile.name
+        user.username = profile.username
+        user.email = profile.email
+        user.save()
+
+
+
+
+
