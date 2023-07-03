65223d592297e4ed0d924c8a4b21b9b5a407affa
Helen <elena.kozovyakina@gmail.com>
2023-07-03 21:55:06 +0300

    devsearch

--- fourth/devsearch/users/views.py
+++ fourth/devsearch/users/views.py
@@ -1,16 +1,17 @@
 from django.shortcuts import render, redirect
-from .models import Profile
+from .models import Profile, Skill
 from django.contrib.auth import logout, login, authenticate
 from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from django.contrib import messages
 from .forms import CustomUserCreationForm, ProfileForm, SkillForm
 from django.contrib.auth.decorators import login_required
+from .utils import search_profiles
 
 
 def profiles(request):
-    prof = Profile.objects.all()
-    context = {'profiles': prof}
+    prof, search_query = search_profiles(request)
+    context = {'profiles': prof, 'search_query': search_query}
     return render(request, 'users/index.html', context)
 
 
