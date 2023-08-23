6dc51a17adcc2c27c9b1285bf1caaefb74b4b1f3
Helen <elena.kozovyakina@gmail.com>
2023-08-23 22:22:46 +0300

    shop

---
+++ seventh/shop/users/forms.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.forms import AuthenticationForm
+from django import forms
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Введите имя пользователя'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Введите пароль'}))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control py-4'
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
