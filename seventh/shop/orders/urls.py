35406d48916db997e747f2732ccdcea391c5a13e
Helen <elena.kozovyakina@gmail.com>
2023-09-06 21:45:36 +0300

    shop

--- seventh/shop/orders/urls.py
+++ seventh/shop/orders/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import OrderCreateView, full_order
+from .views import OrderCreateView, full_order, OrderListView, OrderDetailView
 
 urlpatterns = [
     path('order-create/', OrderCreateView.as_view(), name="order-create"),
     path('full-order/', full_order, name="full-order"),
+    path('', OrderListView.as_view(), name="orders-list"),
+    path('order/<int:pk>/', OrderDetailView.as_view(), name="order"),
 ]
